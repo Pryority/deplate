bbb45265e71124edba76cfdc8752615bbf55b045
pryority <matthewapryor@gmail.com>
2024-03-29 13:06:18 -0400

    ➕ Add scaffold.ts for creating app based on params

--- src/command.ts
+++ src/command.ts
@@ -22,4 +22,23 @@ yargs(hideBin(process.argv))
         typescript: argv.typescript as boolean,
       }),
   )
+  .command(
+    "frontend <framework> <typescript>",
+    "Creates a new Frontend for the Template",
+    (yargs) => {
+      yargs.positional("framework", {
+        description: "The frontend framework of the template",
+        type: "number",
+      });
+      yargs.positional("typescript", {
+        description: "Check if using Typescript or Javascript",
+        type: "boolean",
+      });
+    },
+    (argv) =>
+      create_frontend_handler({
+        framework: argv.framework as number,
+        typescript: argv.typescript as boolean,
+      }),
+  )
   .parse();
---
+++ src/scaffold.ts
@@ -0,0 +1,5 @@
+export function scaffoldAppTemplate(templateData: any) {
+  // Here, you can implement the logic to scaffold your app based on templateData
+  // For example, you might generate files, set up project structure, install dependencies, etc.
+  console.log("Initializing app with template data:", templateData);
+}
