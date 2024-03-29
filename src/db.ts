ec743d253d713268333f51f7ca841a4e6d47cc76
pryority <matthewapryor@gmail.com>
2024-03-29 11:52:39 -0400

    🐛 Fix: remove comma and solve query errors

--- src/db.ts
+++ src/db.ts
@@ -1,4 +1,5 @@
 import { Database } from "bun:sqlite";
+import type { FeParams } from "./types";
 
 const db = new Database("../db.slite");
 db.exec("PRAGMA template_mode = WAL;");
@@ -7,7 +8,7 @@ export const frontend_table_query =
   db.prepare(`CREATE TABLE IF NOT EXISTS frontend (
   frontend_id INTEGER PRIMARY KEY AUTOINCREMENT,
   framework INTEGER NOT NULL,
-  typescript BOOL NOT NULL,
+  typescript BOOL NOT NULL
   )`);
 
 export const backend_table_query =
@@ -21,17 +22,14 @@ export const template_table_query =
   db.prepare(`CREATE TABLE IF NOT EXISTS template (
   template_id INTEGER PRIMARY KEY AUTOINCREMENT,
   frontend_id INTEGER NOT NULL,
-  backend_id INTEGER NOT NULL,
+  backend_id INTEGER NOT NULL
   )`);
-
-type FeParams = {
-  framework: number;
-  typescript: boolean;
-};
 
 export function create_new_frontend(params: FeParams): void {
   const { framework, typescript } = params;
-  const query = db.query(`INSERT INTO frontend (framework) VALUES (?)`);
+  const query = db.query(
+    `INSERT INTO frontend (framework, typescript) VALUES (?, ?)`,
+  );
   query.run(framework, typescript);
 }
 
